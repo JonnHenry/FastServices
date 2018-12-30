b9b68894eaa85f9149d968db62be2597ad9fdfbf
jonnathan <jonnathanca9601@gmail.com>
2018-12-30 03:29:44 -0500

    Routing de todos los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
+import { SolicitarComponent } from './solicitar/solicitar.component';
+import { BuscarComponent } from './buscar/buscar.component';
 
 
 const appRoutes: Routes = [
   {path: '',component : HomeComponent},
   {path: 'login',component : LoginComponent},
-  {path: 'registry',component : RegistroComponent}
+  {path: 'register',component : RegistroComponent},
+  {path: 'buscar',component : BuscarComponent},
+  {path: 'registro',component : RegistroComponent}
     
 ];
 
