efa7339d0f5e26edae78161b0eda64aa8ab10153
Jonnathan Campoberde <jonnathanca9601@gmail.com>
2019-01-28 14:55:38 -0500

    Configuracion hecha

--- src/app/configuracion/configuracion.component.ts
+++ src/app/configuracion/configuracion.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-configuracion',
   templateUrl: './configuracion.component.html',
-  styleUrls: ['./configuracion.component.css']
+  styleUrls: ['bootstrap.min.css',
+    './buscar.component.css',
+    './shop_responsive.css',
+    './shop_styles.css']
 })
 export class ConfiguracionComponent implements OnInit {
 
