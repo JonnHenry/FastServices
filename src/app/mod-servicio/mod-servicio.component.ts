371fa04dd9fabe703f3ae67332fc39b4501da8bb
JEEU95 <jeeu.95@gmail.com>
2019-02-01 16:31:55 -0500

    1

--- src/app/mod-servicio/mod-servicio.component.ts
+++ src/app/mod-servicio/mod-servicio.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewChild } from '@angular/core';
-import {  } from '@types/googlemaps';
+
 
 @Component({
   selector: 'app-mod-servicio',
@@ -9,24 +9,11 @@ import {  } from '@types/googlemaps';
 })
 export class ModServicioComponent implements OnInit {
 
-  @ViewChild('gmap') gmapElement: any;
-  map: google.maps.Map;
-
-
   constructor() { }
 
   ngOnInit() {
-    // tslint:disable-next-line:prefer-const
-    let mapProp = {
-      center: new google.maps.LatLng(18.5793, 73.8143),
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+   
   }
 
-  setMapType(mapTypeId: string) {
-    this.map.setMapTypeId(mapTypeId);
-  }
 
 }
