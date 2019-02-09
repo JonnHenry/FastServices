d505541801ebb640b1f4064e7509277984d033c2
Jonnathan Campoberde <jonnathanca9601@gmail.com>
2019-02-09 09:43:10 -0500

    Ubicacion del usuario corecta

--- src/app/mod-servicio/mod-servicio.component.ts
+++ src/app/mod-servicio/mod-servicio.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, NgZone } from '@angular/core';
 import { AppComponent } from '../app.component';
-import { FileUploader, FileUploaderOptions, ParsedResponseHeaders } from 'ng2-file-upload';
-import { Cloudinary } from '@cloudinary/angular-5.x';
 import { Router } from '@angular/router';
 import { UbicacionService } from '../ubicacion/ubicacion.service';
 import * as Leaflet from 'leaflet';
@@ -12,12 +10,11 @@ import * as Leaflet from 'leaflet';
   styleUrls: ['./mod-servicio.component.css'],
 })
 export class ModServicioComponent implements OnInit {
-  public ubicaciones=[];
-  private uploader: FileUploader;
+  public ubicaciones = [];
   private title: string;
 
   constructor(private router: Router,
-    private appComponent: AppComponent,private _ubicacionService: UbicacionService) {
+    private appComponent: AppComponent, private _ubicacionService: UbicacionService) {
      }
 
   ngOnInit() {
@@ -27,53 +24,53 @@ export class ModServicioComponent implements OnInit {
     this.appComponent.cerrarSesion();
     this.router.navigate(['login']);
   }
-  findMe(){
-    var output = document.getElementById('map');
+  findMe() {
+    const output = document.getElementById('map');
     this._ubicacionService.getUbicacion()
-  .subscribe(data => this.ubicaciones =data);
-  
+  .subscribe(data => this.ubicaciones = data);
+
 
     // Verificar si soporta geolocalizacion
     if (navigator.geolocation) {
-      output.innerHTML = "<p>Geolocalizacion Activa</p>";
-    }else{
-      output.innerHTML = "<p>Tu navegador no soporta Geolocalizacion</p>";
+      output.innerHTML = '<p> Geolocalizacion Activa </p>';
+    } else {
+      output.innerHTML = '<p>Tu navegador no soporta Geolocalizacion</p>';
     }
 
 
-    //Obtenemos latitud y longitud
-    function localizacion(posicion){
+    // Obtenemos latitud y longitud
+    function localizacion(posicion) {
 
-      var latitude = posicion.coords.latitude;
-      var longitude = posicion.coords.longitude;
-     
-      output.innerHTML ="<div>Lat: " + latitude+",  Long: "+longitude+"</div>";
-      
-      
-      drawMap(latitude,longitude);
+      const latitude = posicion.coords.latitude;
+      const longitude = posicion.coords.longitude;
+
+      output.innerHTML = '<div>Lat: ' + latitude + ', Long: ' + longitude + '</div>';
+
+
+      drawMap(latitude, longitude);
     }
 
-    function error(){
-      output.innerHTML = "<p>No se pudo obtener tu ubicación</p>";
+    function error() {
+      output.innerHTML = '<p>No se pudo obtener tu ubicación</p>';
     }
-    function drawMap(lat,lon) {
-      let mapa = Leaflet.map('mapa').setView([lat,lon], 15);
+    function drawMap(lat, lon) {
+      const mapa = Leaflet.map('mapa').setView([lat, lon], 15);
       Leaflet.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: 'Ubicacion',     
+        attribution: 'Ubicacion',
         maxZoom: 18,
         minZoom: 6
       }).addTo(mapa);
       Leaflet.marker([lat, lon]).addTo(mapa)
       .bindPopup('Mi Ubicación')
       .openPopup();
-  
-      //alert on location error
+
+      // alert on location error
       function onLocationError(e) {
         alert(e.message);
       }
-  
+
       mapa.on('locationerror', onLocationError);
     }
-    navigator.geolocation.getCurrentPosition(localizacion,error);   
+    navigator.geolocation.getCurrentPosition(localizacion,error);
   }
 }
